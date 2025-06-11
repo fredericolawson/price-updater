f51a3a96e85bf8d1b1bbc90b680fc5fd6f900a9f
Frederick Lawson <fredjlawson@gmail.com>
2025-06-11 10:53:11 -0300

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,6 @@ const eslintConfig = [
     rules: {
       '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/no-unused-vars': 'warn',
-      'prettier/prettier': 'warn',
     },
   },
 ];
